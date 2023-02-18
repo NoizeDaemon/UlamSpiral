af901b6e6c50773e2a719ae31c4463ae4b189fca
NoizeDaemon <freidario@gmx.at>
2023-02-18 05:50:21 +0100

    Expanded working sample (RelativePanel outside ItemsControl)

--- UlamSpiral/EasyAlignWith.cs
+++ UlamSpiral/EasyAlignWith.cs
@@ -120,16 +120,6 @@ namespace UlamSpiral
         public static readonly AttachedProperty<object> TargetProperty =
             AvaloniaProperty.RegisterAttached<RelativePanel, Layoutable, object>("Target");
 
-        public static Func<AvaloniaObject, object, object> CoerceTarget = delegate (AvaloniaObject obj, object valueIn)
-        {
-            if (valueIn is string)
-            {
-                var target = ((Control)obj).FindControl<Control>((string)valueIn);
-                return target ?? AvaloniaProperty.UnsetValue;
-            }
-            else return AvaloniaProperty.UnsetValue;
-        };
-
         /////////////
         /// RIGHT ///
         /////////////
